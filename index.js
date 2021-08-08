7a74f33f2540f3aa8c488684aed02a1fd29a754a
Filip Piptorošević <filip.pipta@gmail.com>
2021-08-08 16:10:09 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://Filip:gn951753@movieapp.ns5nu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+                                                                                                                        .then(() => console.log('DB Connected'))
+                                                                                                                        .catch(err => console.error(err));
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
+ 
+app.listen(5000);
