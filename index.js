cbe6784e9cff4fed64a81d55d2913ee0e3a519cc
Filip Piptorošević <piptorosevic.it11.2019@uns.ac.rs>
2021-08-11 20:33:26 +0200

    safe

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {User} = require('./models/user');
 const {auth} = require('./middleware/auth');
 
 
-mongoose.connect(config.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(config.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true,  useFindAndModify: false })
 .then(() => console.log('DB Connected'))
 .catch(err => console.error(err));
 
@@ -25,11 +25,11 @@ app.get('/api/user/auth', auth, (req, res) => {
         name: req.user.name,
         lastname: req.user.lastname,
         role: req.user.role
-    })
-})
+    });
+});
 
 app.post('/api/users/register', (req, res) => {
-    const user = new User(req.body)
+    const user = new User(req.body);
 
     user.save((err, doc) => {
         if(err) return res.json( {success: false, err} );
@@ -38,25 +38,34 @@ app.post('/api/users/register', (req, res) => {
             success: true,
             userData: doc
         });
-    }) 
-})
+    });
+});
 
 app.post('/api/user/login', (req, res) => {
+
     User.findOne({email: req.body.email}, (err, user) => {
         if(!user) return res.json({loginSuccess: false, message: "Email not found"});
 
         user.comparePassword(req.body.password, (err, isMatch) => {
             if(!isMatch) return res.json({loginSuccess: false, message: "Wrong password"});
-        })
+        });
 
         user.generateToken((err, user) => {
             if(err) return res.status(400).send(err);
 
             res.cookie("x_auth", user.token).status(200).json({loginSuccess: true});
-        })
+        });
+    });
+});
+
+
+app.get('/api/users/logout', auth, (req, res) => {
+
+    User.findOneAndUpdate({_id: req.user._id}, {token: ''}, (err, doc) => {
+        if(err) return res.json({success: false, err});
+        return res.status(200).send({success: true});
     })
 })
-
 
 
 app.listen(5000);
