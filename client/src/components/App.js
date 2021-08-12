3b0aa5d7b2e70d7a086c4e52c034c655c2260bdb
Filip Piptorošević <piptorosevic.it11.2019@uns.ac.rs>
2021-08-12 19:54:37 +0200

    redux

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import { Route, Switch } from "react-router-dom";
+import About from "./about";
+
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route path ="/about" component = {About} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
