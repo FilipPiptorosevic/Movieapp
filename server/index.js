5bc0f839401ebf77c7b9738442cc4218af3e9409
Filip Piptorošević <piptorosevic.it11.2019@uns.ac.rs>
2021-08-30 15:15:52 +0200

    final

--- server/index.js
+++ server/index.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const path = require("path");
-//const {User} = require('./models/user');
-//const {auth} = require('./middleware/auth');
 
 
 mongoose.connect(config.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true,  useFindAndModify: false })
@@ -29,63 +27,6 @@ if(process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
-
-
-
-/*app.get('/api/users/auth', auth, (req, res) => {
-    res.status(200).json({
-        _id: req._id,
-        isAdmin: req.user.role === 0 ? false : true,
-        isAuth: true,
-        email: req.user.email,
-        name: req.user.name,
-        lastname: req.user.lastname,
-        role: req.user.role
-    });
-});
-
-app.post('/api/users/register', (req, res) => {
-    const user = new User(req.body);
-
-    user.save((err, doc) => {
-        if(err) return res.json( {success: false, err} );
-
-        return res.status(200).json({
-            success: true,
-            userData: doc
-        });
-    });
-});
-
-app.post('/api/users/login', (req, res) => {
-
-
-    User.findOne({email: req.body.email},function (err, user) {
-        if(!user) {
-            return res.json({loginSuccess: false});
-        } else {
-            user.comparePassword(req.body.password, (err, isMatch) => {
-                if(!isMatch) {
-                    return res.json({loginSuccess: false});
-                } else {
-                    user.generateToken((err, user) => {
-                        if(err) return res.status(400).send(err);
-                        res.cookie("x_auth", user.token).status(200).json({loginSuccess: true});
-                    });
-                }
-            });
-        }
-    });
-})
-
-
-app.get('/api/users/logout', auth, (req, res) => {
-
-    User.findOneAndUpdate({_id: req.user._id}, {token: '', tokenExp: ''}, (err, doc) => {
-        if(err) return res.json({success: false, err});
-        return res.status(200).send({success: true});
-    })
-})*/
 
 
 const port = process.env.PORT || 5000
